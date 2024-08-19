f4831883b814ddc90b997f0688ad68b4c2db39f8
“Kuldeep <kuldeepwadhwa9@gmail.com>
2024-08-19 17:38:19 +0100

    refactored code

--- src/main/scala/GuessWhoGame.scala
+++ src/main/scala/GuessWhoGame.scala
@@ -13,36 +13,29 @@ object GuessWhoGame extends App {
 
 
   @tailrec
-  def gameLoop(remainingCharacters: List[Character]): Unit = {
+  private def gameLoop(remainingCharacters: List[Character]): Unit = {
     if (remainingCharacters.size > 1) {
+      println("\nRemaining characters: " + remainingCharacters.map(_.name).mkString(", "))
+      println("Enter an attribute to guess the character (options: 'hat', 'glasses', 'gender'):")
 
-      println(s"\nRemaining characters: ${remainingCharacters.map(_.name).mkString(", ")}")
-      println("\nEnter an attribute for example, '(hat, glasses, gender)' to guess the character:")
+      val question = readLine().trim.toLowerCase
+      val updatedCharacters = question match {
+        case "hat" =>
+          val answer = askYesNoQuestion("Does the character wear a hat?")
+          GameLogic.filterCharacterBasedOnAttributes(remainingCharacters, "hat", answer)
+        case "glasses" =>
+          val answer = askYesNoQuestion("Does the character wear glasses?")
+          GameLogic.filterCharacterBasedOnAttributes(remainingCharacters, "glasses", answer)
+        case "gender" =>
+          val gender = askGenderQuestion("Is the character male or female?")
+          GameLogic.filterCharacterBasedOnAttributes(remainingCharacters, "gender", gender == "male")
+        case _ =>
+          println("Unrecognized attribute. Please try again.")
+          remainingCharacters
+      }
 
-      val question: String = readLine()
-      println(s"Raw input: $question")
-      val answer: Boolean = question.contains("yes")
-
-      val updatedCharacters = if (question.contains("hat")) {
-        GameLogic.filterCharacterBasedOnAttributes(remainingCharacters, "hat", answer)
-      } else if (question.contains("glasses")) {
-        GameLogic.filterCharacterBasedOnAttributes(remainingCharacters, "glasses", answer)
-      }
-      else if (question.contains("gender")) {
-        println("Enter the gender Male/ Female")
-        val genderQuestion: String = readLine()
-        println(s"Raw input: $genderQuestion")
-        val answer: Boolean = if (genderQuestion.contains("Male")) true else false
-        GameLogic.filterCharacterBasedOnAttributes(remainingCharacters, "gender", answer)
-      }
-      else {
-        println("Question not recognized. Please ask about 'hat', 'glasses', or 'gender'.")
-        remainingCharacters
-      }
       gameLoop(updatedCharacters)
-    }
-    else
-    {
+    } else {
       println(s"\nYour guess: ${remainingCharacters.head.name}")
       if (remainingCharacters.head == chosenCharacter) {
         println("Congratulations! Your guess was correct.")
@@ -51,6 +44,17 @@ object GuessWhoGame extends App {
       }
     }
   }
+
+  private def askYesNoQuestion(question: String): Boolean = {
+    println(question + " (yes/no)")
+    readLine().trim.toLowerCase == "yes"
+  }
+
+  private def askGenderQuestion(question: String): String = {
+    println(question + " (male/female)")
+    readLine().trim.toLowerCase
+  }
+
 
   gameLoop(characters)
 }
